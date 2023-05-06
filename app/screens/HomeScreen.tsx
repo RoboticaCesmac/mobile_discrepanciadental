e0513730527ff961efdedb425c93f334c64663f1
Eugenio <eugeniolimaeugenio5000@outlook.com>
2023-05-06 00:16:54 -0300

    Adicionando get para buscar documentos da coleção pacientes
    O hook chamado useEffect está sendo usado para ser executado quando o componente é carregado. O segundo parametro, um array vazio, passado para ele diz para ele ser executado somente uma vez.

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,28 +1,67 @@
-import { Alert, Button, SafeAreaView, Text } from "react-native"
-import { signOut } from 'firebase/auth';
+import {
+  Alert,
+  SafeAreaView,
+  Text,
+  Button,
+} from "react-native";
+import { signOut } from "firebase/auth";
+import { auth, database } from "@config/firebase";
+import styles from './styles';
+import { useEffect, useState } from "react";
+import {
+  DocumentData,
+  QuerySnapshot,
+  collection,
+  getDocs,
+} from "firebase/firestore";
+import React from "react";
 
-import { auth } from '@config/firebase';
-import styles from "./styles"
+const HomeScreen = ({ navigation }: any) => {
 
-const HomeScreen = ({navigation}) =>{
-    var name = auth.currentUser?.email;
-    const logout = async () => {
-        try {
-            await signOut(auth);
-        } catch(e) {
-            Alert.alert('Ocorreu um erro', JSON.stringify(e))
-        } finally {
-            navigation.navigate('Authentication');
-        }
+
+  useEffect(() => {
+    getDocs<DocumentData>(collection(database, "patients"))
+      .then((docs: QuerySnapshot<DocumentData>) => {
+        let patients: any[] = [];
+        docs.forEach((doc: DocumentData) => {
+          //console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+          //console.log(`${doc.id} => ${doc.data().name}`);
+          const patient = {
+            id: doc.id,
+            name: doc.data().name,
+            cpf: doc.data().cpf,
+          };
+          patients.push(patient);
+        });
+        console.log(patients);
+      })
+      .catch((e) => {
+        console.log("Home, getUsers: " + e);
+      });
+  }, []);
+
+
+  var name = auth.currentUser?.email;
+  const logout = async () => {
+    try {
+      await signOut(auth);
+    } catch (e) {
+      Alert.alert("Ocorreu um erro", JSON.stringify(e));
+    } finally {
+      navigation.navigate("Authentication");
     }
-    
-    return(
-        <SafeAreaView style={{ flex: 1, alignItems:'center', backgroundColor: "white" }}>
-            <Text style={styles.header}>{name}</Text>
-            <Button title="Log out" onPress={logout} />
-            <Button title="Go Back" onPress={() => navigation.navigate('Authentication')} />
-        </SafeAreaView>
-    )
-}
+  };
+
+  return (
+    <SafeAreaView
+      style={{ flex: 1, alignItems: "center", backgroundColor: "white" }}
+    >
+      <Text style={styles.header}>{name}</Text>
+
+      <Button title="Log out" onPress={logout} />
+      <Button title="Go Back" onPress={() => navigation.navigate('Authentication')} />
+    </SafeAreaView>
+  );
+};
 
 export { HomeScreen };
