aece7585667cc484e615662119f54eae9c5d90d1
Eugenio <eugeniolimaeugenio5000@outlook.com>
2023-05-14 03:09:40 -0300

    Fix string de validacao

--- app/screens/Validation.tsx
+++ app/screens/Validation.tsx
@@ -7,12 +7,12 @@ export const RegisterPatientValidation = Yup.object().shape({
     .required("Campo obrigatório"),
   firstName: Yup.string()
     .required("Campo obrigatório")
-    .max(15, "O primeiro nome deve possuir no máximo 15 caracteres")
-    .min(3, "O primeiro nome deve possuir no mínimo 3 caracteres"),
+    .max(15, "O nome deve possuir no máximo 15 caracteres")
+    .min(3, "O nome deve possuir no mínimo 3 caracteres"),
   surName: Yup.string()
     .required("Campo obrigatório")
     .max(15, "O sobrenome deve possuir no máximo 15 caracteres")
-    .min(3, "O sobrenome nome deve possuir no mínimo 3 caracteres"),
+    .min(3, "O sobrenome deve possuir no mínimo 3 caracteres"),
 });
 export const AuthValidation = Yup.object().shape({
   email: Yup.string()
