5fe86d5d109332cf406320caffdb5bd43ac7ed4c
Eugenio <eugeniolimaeugenio5000@outlook.com>
2023-05-13 21:19:46 -0300

    Adicionando validacao

--- app/screens/Validation.tsx
+++ app/screens/Validation.tsx
@@ -1,7 +1,19 @@
 import * as Yup from "yup";
 
 // https://github.com/jquense/yup
-
+export const RegisterPatientValidation = Yup.object().shape({
+  email: Yup.string()
+    .email("Digite um email válido")
+    .required("Campo obrigatório"),
+  firstName: Yup.string()
+    .required("Campo obrigatório")
+    .max(15, "O primeiro nome deve possuir no máximo 15 caracteres")
+    .min(3, "O primeiro nome deve possuir no mínimo 3 caracteres"),
+  surName: Yup.string()
+    .required("Campo obrigatório")
+    .max(15, "O sobrenome deve possuir no máximo 15 caracteres")
+    .min(3, "O sobrenome nome deve possuir no mínimo 3 caracteres"),
+});
 export const AuthValidation = Yup.object().shape({
   email: Yup.string()
     .email("Digite um email válido")
